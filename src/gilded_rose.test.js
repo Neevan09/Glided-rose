a60d7aefc36df9b86a481494cf318dfd9db63e51
naveen sampath <naveensampath09@gmail.com>
2020-09-03 16:23:39 -0500

    TDD - Test Failed.

--- src/gilded_rose.test.js
+++ src/gilded_rose.test.js
@@ -5,33 +5,44 @@
    items
  } = require('./gilded_rose')
 
- describe('update_quality', function () {
-
+ describe('update_quality', (() => {
    it('should be a function', function () {
      expect(typeof update_quality).toBe("function");
    });
+ }));
 
- });
 
-
- describe('items', function () {
+ describe('items', (() => {
    it('should have items object', function () {
      expect(typeof items).toBe("object");
    });
 
-   it('should have items', function () {
+   it('should have items', (() => {
      expect(items.length).toEqual(6);
-   });
+    }));
 
    //Sell_in value
-   it('All items should have sell_in value', function () {
+   it('All items should have sell_in value', (() => {
      const itemArr = items.map(item => item.sell_in)
      expect(itemArr).not.toBeNull();
-   });
+    }));
 
    //Quality
-   it('All items should have Quality value', function () {
+   it('All items should have Quality value', (() => {
      const qualityArr = items.map(item => item.quality)
      expect(qualityArr).not.toBeNull();
-   });
- });
+    }));
+
+   //sell_in days is less then zero, quality degrades twice as fast
+   it('All items should have sell_in less than zero', (() => {
+    const sell_in = items.map(item => item.sell_in)
+    expect(sell_in.some(({value}) => value < 0)).toBe(true);
+  }));
+
+  //  //The quality of an item is never negative
+  //  it('quality of an item should never negative', (() => {
+  //   const sell_in = items.map(item => item.sell_in)
+  //   expect(sell_in.map(value => value < 0)).toBeLessThan(0);
+  // }));
+
+}));

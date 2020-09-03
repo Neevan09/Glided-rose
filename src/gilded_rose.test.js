6259d1de9ba5937fce196b9abd4ab0f9af1e8eae
naveen sampath <naveensampath09@gmail.com>
2020-09-03 14:45:00 -0500

    TDD - Test Passed.

--- src/gilded_rose.test.js
+++ src/gilded_rose.test.js
@@ -1,3 +1,5 @@
+ const update_quality = require('./gilded_rose')
+
  describe('update_quality', function() {
   it('should be a function', function() {
     expect(typeof update_quality).toBe("function");

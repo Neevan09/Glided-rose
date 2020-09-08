c9808e8185477b95e0bd289fe665f131b69c66bf
naveen sampath <naveensampath09@gmail.com>
2020-09-08 12:23:51 -0500

    TDD - Code refactored and optimised using ES6 features.

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,11 +1,13 @@
-function Item(name, sell_in, quality) {
+// CC: funciton Intialization.
+let Item = function (name, sell_in, quality) {
   this.name = name;
   this.sell_in = sell_in;
   this.quality = quality;
 }
 
-var items = []
+let items = [];
 
+// CC: Adding new items.
 items.push(new Item('+5 Dexterity Vest', 10, 20));
 items.push(new Item('Aged Brie', 2, 0));
 items.push(new Item('Elixir of the Mongoose', 5, 7));
@@ -14,49 +16,23 @@ items.push(new Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
 items.push(new Item('Conjured Mana Cake', 3, 6));
 
 const update_quality = () => {
-  for (var i = 0; i < items.length; i++) {
-    let item = items[i];
-    if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1;
-      }
-    } else {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if ((item.sell_in < 11) && (item.quality < 50)) {
-            item.quality = item.quality + 1;
-          }
-          if ((item.sell_in < 6) && (item.quality < 50)) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    }
-    if (item.name != 'Sulfuras, Hand of Ragnaros') {
-      item.sell_in = item.sell_in - 1;
-    }
-    if (item.sell_in < 0) {
-      if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1
-        } else {
-          item.quality = item.quality - item.quality
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-        }
-      }
-    }
-  }
-}
 
-// module.exports = update_quality;
-// module.exports = {
-//   update_quality:function() {},
-//   // items: []
-// }
+  items.map(item => {
+    //First Condition.
+    (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') ?
+    (item.quality > 0 ? item.quality = item.quality - 1 : '') :
+    (item.quality < 50 && item.name == 'Backstage passes to a TAFKAL80ETC concert') ? ((item.sell_in < 11 || item.sell_in < 6) ? item.quality = item.quality + 1 : '') : ''
+   
+    //Second Condition.
+    (item.name != 'Sulfuras, Hand of Ragnaros') ? item.sell_in = item.sell_in - 1: '';
+   
+    //Third Condition.
+    (item.sell_in < 0) ?
+    (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert' && item.name != 'Sulfuras, Hand of Ragnaros') ?
+    ((item.quality > 0) ? item.quality = item.quality - 1 : item.quality - item.quality) :
+    ((item.quality < 50) ? item.quality = item.quality + 1 : '') : ''
+  })
+}
 
 exports.update_quality = function () {};
 exports.items = items;
